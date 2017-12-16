245e1eabf90ba56ff38b8e1f8c018ebb32089aa1
zhouxiaotian <1282347298@qq.com>
2017-12-16 11:23:35 +0800

    封装myQueryURLParameter方法，方便后期获取地址栏中的参数值

--- WEEK8/4-VOTE/js/common.js
+++ WEEK8/4-VOTE/js/common.js
@@ -37,3 +37,29 @@ let cookie = (function () {
         remove: removeValue
     }
 })();
+
+/*--QUERY URL--*/
+~function () {
+    let pro = String.prototype;
+
+    //=>获取URL问号传递的参数值
+    pro.myQueryURLParameter = function myQueryURLParameter() {
+        let obj = {},
+            reg = /[?&#]([^?#&=]+)(?:=([^?#&=]*))?/g;
+        this.replace(reg, (...arg)=> {
+            /*
+             * RES：本次大正则匹配的结果
+             * KEY：本次第一个分组捕获的内容(未来做为对象的属性名)
+             * VALUE：本次第二个分组捕获的内容(未来做为对象的属性值)
+             */
+            let [res,key,value]=arg;
+            if (res.indexOf('#') > -1) {
+                //->这个是HASH的处理
+                obj['HASH'] = key;
+                return;
+            }
+            obj[key] = value;
+        });
+        return obj;
+    };
+}();
